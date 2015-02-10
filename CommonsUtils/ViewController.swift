ca37f4aa0e087b55b0672b289b87acce2f4f81fc
yasu <yasu@MacBookAir.local>
2015-02-11 03:19:06 +0900

    first commit

---
+++ CommonsUtils/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CommonsUtils
+//
+//  Created by yasu on 2015/02/11.
+//  Copyright (c) 2015年 ho9ho9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
